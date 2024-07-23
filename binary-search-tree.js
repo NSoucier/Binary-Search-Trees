a4058c3ae65cbf34a7c4724f3ad3c497986a0688
Nathan Soucier <soucienp@gmail.com>
2024-07-23 17:32:09 -0400

    completed methods for tree, except BFS

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -27,7 +27,6 @@ class BinarySearchTree {
           current = current.right;
         } else {
           current.right = new Node(val);
-          console.log(current.right.val)
           return this.root;
         }
       } else {
@@ -44,37 +43,139 @@ class BinarySearchTree {
   /** insertRecursively(val): insert a new node into the BST with value val.
    * Returns the tree. Uses recursion. */
 
-  insertRecursively(val) {}
+  insertRecursively(val, current = this.root) {
+    if (current === null) {
+      this.root = new Node(val);
+      return this.root
+    }
+
+    if (val > current.val) {
+      if (current.right === null) {
+        current.right = new Node(val);
+        return this.root
+      }
+      current = this.insertRecursively(val, current.right);
+    } else {
+      if (current.left === null) {
+        current.left = new Node(val);
+        return this.root
+      }
+      current = this.insertRecursively(val, current.left);
+    }
+  }
 
   /** find(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses iteration. */
 
-  find(val) {}
+  find(val) {
+    if (this.root.val === val) return this.root
+
+    let current = this.root
+    while (current) {
+      if ( val > current.val ) {
+        current = current.right
+      } else if ( val < current.val ) {
+        current = current.left
+      } else if ( val === current.val) {
+        return current;
+      }
+    }
+
+    return undefined
+  }
 
   /** findRecursively(val): search the tree for a node with value val.
    * return the node, if found; else undefined. Uses recursion. */
 
-  findRecursively(val) {}
+  findRecursively(val, current = this.root) {
+    if (current.val === val) return current
+
+    if (val > current.val) {
+      if (current.right === null) return undefined
+      current = this.findRecursively(val, current.right);
+      return current
+    } else {
+      if (current.left === null) return undefined
+      current = this.findRecursively(val, current.left)
+      return current
+    }
+  }
 
   /** dfsPreOrder(): Traverse the array using pre-order DFS.
    * Return an array of visited nodes. */
 
-  dfsPreOrder() {}
+  dfsPreOrder() {
+    // traverse myself, left, then right
+    const result = [];
+
+    function traverse(current) {
+      result.push(current.val);
+      if (current.left !== null) traverse(current.left)
+      if (current.right !== null) traverse(current.right)
+    }
+
+    traverse(this.root);
+
+    return result
+  }
 
   /** dfsInOrder(): Traverse the array using in-order DFS.
    * Return an array of visited nodes. */
 
-  dfsInOrder() {}
+  dfsInOrder() {
+    // traverse left, myself, then right
+    const result = [];
+
+    function traverse(current) {
+      if (current.left !== null) traverse(current.left)
+      result.push(current.val);
+      if (current.right !== null) traverse(current.right)
+    }
+
+    traverse(this.root);
+
+    return result
+  }
 
   /** dfsPostOrder(): Traverse the array using post-order DFS.
    * Return an array of visited nodes. */
 
-  dfsPostOrder() {}
+  dfsPostOrder() {
+    // travers left, right, then myself
+    const result = [];
+
+    function traverse(current) {
+      if (current.left !== null) traverse(current.left)
+      if (current.right !== null) traverse(current.right)
+      result.push(current.val);
+    }
+
+    traverse(this.root);
+
+    return result
+  }
 
   /** bfs(): Traverse the array using BFS.
    * Return an array of visited nodes. */
 
-  bfs() {}
+  bfs() {
+    const result = [];
+    const queue = [this.root];
+    let current = this.root;
+
+    while (queue.length) {
+      result.push(queue[0].val);
+      if (current.left !== null) queue.push(current.left)
+      if (current.right !== null) queue.push(current.right)
+      
+      // check if this works
+
+      queue.shift();
+    }
+
+    console.log(result)
+    return result
+  }
 
   /** Further Study!
    * remove(val): Removes a node in the BST with the value val.
